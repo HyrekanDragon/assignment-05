e250cf21a6f2dde0ac4fcc07e26a22701abccd80
HyrekanDragon <HyrekanDragon@yahoo.com>
2016-04-22 10:21:00 -0700

    Create Constants.h

---
+++ Solution/HyrekanDragon/Constants.h
@@ -0,0 +1,18 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chuoy <HyrekanDragon@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* A file for holding constants.
+*/
+
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H	
+
+//Terminal Size
+const int X = 79;
+const int Y = 25;
+
+#endif
